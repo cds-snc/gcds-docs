36d4049c453fbe083b81f58961728abdd03b8b38
Melanie Boeckmann <melanie.bockmann@gmail.com>
2022-03-31 13:22:52 -0700

    feat: add experimental banner above header

---
+++ src/_data/experimentalbanner.js
@@ -0,0 +1,8 @@
+module.exports = {
+    en : {
+        bannerText: "This tool is currently being piloted on a small scale. Contact us if you'd like to pilot this tool in your service.",
+    },
+    fr: {
+        bannerText: "Cet outil fait actuellement l'objet d'un petit projet pilote. Contactez-nous si vous souhaitez essayer l'outil dans votre service.",
+    }
+}
