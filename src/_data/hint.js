4cbfb1486b73422a82836fe1494978db0e3c1ba8
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-02-08 14:59:22 -0500

    Add loops and arrays for hint and error-message partials

--- src/_data/hint.js
+++ src/_data/hint.js
@@ -1,18 +1,18 @@
 module.exports = {
   en : {
     heading: "Keep hint text static and perceivable",
-    listitems: {
-      0: "Add hint text to help a person provide a complete value in the input and avoid an error state.",
-      1: "Use the hint text attribute to add the hint text below the label and above the field.",
-      2: "Avoid placing hint text in the container where it will disappear once the field is selected.",
-    }
+    listitems: [
+      "Add hint text to help a person provide a complete value in the input and avoid an error state.",
+      "Use the hint text attribute to add the hint text below the label and above the field.",
+      "Avoid placing hint text in the container where it will disappear once the field is selected.",
+    ]
   },
   fr: {
     heading: "Keep hint text static and perceivable",
-    listitems: {
-      0: "Add hint text to help a person provide a complete value in the input and avoid an error state.",
-      1: "Use the hint text attribute to add the hint text below the label and above the field.",
-      2: "Avoid placing hint text in the container where it will disappear once the field is selected.",
-    }
+    listitems: [
+      "Add hint text to help a person provide a complete value in the input and avoid an error state.",
+      "Use the hint text attribute to add the hint text below the label and above the field.",
+      "Avoid placing hint text in the container where it will disappear once the field is selected.",
+    ]
   }
 }
