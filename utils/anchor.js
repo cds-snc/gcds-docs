beb747b4d14a00dce95c1cef5f73b5911be8a10b
Philippe Caron <amazingphilippe@gmail.com>
2023-02-09 15:09:42 -0500

    formatitin

--- utils/anchor.js
+++ utils/anchor.js
@@ -1,23 +1,23 @@
 const slugify = require("./slugify");
 
 const defaultOptions = {
-  anchorClass: "anchor-link",
+  anchorClass: 'anchor-link',
 };
 
 const anchor = (md, options) => {
   options = Object.assign({}, defaultOptions, options);
 
-  md.renderer.rules.heading_open = function (tokens, index) {
+  md.renderer.rules.heading_open = function(tokens, index) {
     const contentToken = tokens[index + 1];
     const slug = slugify(contentToken.content);
 
-    if (tokens[index].tag === "h1") {
+    if (tokens[index].tag === 'h1') {
       return `<${tokens[index].tag} id="${slug}" class="mb-400">`;
-    } else if (tokens[index].tag === "h2") {
+    } else if (tokens[index].tag === 'h2') {
       return `<${tokens[index].tag} id="${slug}" class="mt-500 mb-400">`;
-    } else if (tokens[index].tag === "h3") {
+    } else if (tokens[index].tag === 'h3') {
       return `<${tokens[index].tag} id="${slug}" class="mt-500 mb-400">`;
-    } else if (tokens[index].tag === "h4") {
+    } else if (tokens[index].tag === 'h4') {
       return `<${tokens[index].tag} id="${slug}" class="mt-500 mb-400">`;
     } else {
       return `<${tokens[index].tag} id="${slug}">`;
