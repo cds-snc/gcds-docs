4e4174ae07d2d67861d7829b30ed14cd94ff6a16
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-02-10 15:53:12 -0500

    Add right paragraph and translation

--- src/_data/lookingfor.js
+++ src/_data/lookingfor.js
@@ -1,7 +1,8 @@
 module.exports = {
   en : {
     heading: "Looking for another component?",
-    paragraph: "Check out our <a href=\"/en/components/\">component page</a>. If the component's not there, send us feedback on what you'd like us to work on next.",
+    coparagraph: "Check out our <a href=\"/en/components/\">component page</a>. If the component's not there, send us feedback on what you'd like us to work on next.",
+    csparagraph: "Share your feedback with us to help improve GC Design System or you can check out what's coming soon.",
     feedback: "Give feedback",
     feedbackHref: "/en/contact/#contact-us",
     gotocomponents: "Go to Component overview",
@@ -10,13 +11,14 @@ module.exports = {
     gotocomingsoonhref: "/en/coming-soon/"
   },
   fr: {
-    heading: "Looking for another component?",
-    paragraph: "Check out our <a href=\"/en/components/\">component page</a>. If the component's not there, send us feedback on what you'd like us to work on next.",
+    heading: "Vous cherchez un autre composant?",
+    coparagraph: "Rendez vous sur notre <a href=\"/fr/composants/\">page de composants</a>. S'il y manque un composant, écrivez nous pour nous faire savoir les composants que vous souhaitez voir dans le futur.",
+    csparagraph: "Faites-nous part de vos commentaires pour l'amélioration de Système de design GC ou découvrez nos futures fonctionnalités.",
     feedback: "Fournir des commentaires",
     feedbackHref: "/fr/contactez/#contactez-nous",
-    gotocomponents: "Go to Component overview",
-    gotocomponentshref: "/en/components/",
-    gotocomingsoon: "Go to Coming soon",
-    gotocomingsoonhref: "/en/coming-soon/"
+    gotocomponents: "Passer au survol des composants",
+    gotocomponentshref: "/fr/composants/",
+    gotocomingsoon: "Découvrir les prochaines nouveautés",
+    gotocomingsoonhref: "/fr/developpement-en-cours/"
   }
 }
