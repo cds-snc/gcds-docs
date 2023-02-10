e8158475eb989e4d4c42b0ace6a9b7233db4a12b
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-02-10 12:57:03 -0500

    Fix indent in documentation file

--- src/_data/documentation.js
+++ src/_data/documentation.js
@@ -1,18 +1,18 @@
 module.exports = {
-    en : {
-        usecase: "Use case",
-        usage: "Usage",
-        design: "Design",
-        code: "Code",
-        tokens: "Tokens",
-        "othernames": "Other names: "
-    },
-    fr: {
-        usecase: "Cas d'utilisation",
-        usage: "Usage",
-        design: "Design",
-        code: "Code",
-        tokens: "Tokens",
-        "othernames": "Autres noms : "
-    }
+  en : {
+    usecase: "Use case",
+    usage: "Usage",
+    design: "Design",
+    code: "Code",
+    tokens: "Tokens",
+    othernames: "Other names: "
+  },
+  fr: {
+    usecase: "Cas d'utilisation",
+    usage: "Usage",
+    design: "Design",
+    code: "Code",
+    tokens: "Tokens",
+    othernames: "Autres noms : "
+  }
 }
