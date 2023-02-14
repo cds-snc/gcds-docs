e26273b2b5d075c37596b24791c88e5c520ecd06
Melanie Boeckmann <melanie.bockmann@gmail.com>
2023-02-13 22:14:48 -0800

    improve home hero display

---
+++ src/_data/homehero.js
@@ -0,0 +1,14 @@
+module.exports = {
+  en : {
+    heading: "Canadian Digital Service GC Design System",
+    paragraph: "Build modern, accessible, human government services people can use.",
+    link: "/en/contact/#about-gc-design-system",
+    linkText: "About us",
+  },
+  fr: {
+    heading: "Service numérique canadien Système de design GC",
+    paragraph: "Créez des services gouvernementaux modernes accessibles et humains destinés au public.",
+    link: "/fr/contactez/#a-propos-de-systeme-de-design-gc",
+    linkText: "À propos",
+  }
+}
