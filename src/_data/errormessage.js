620b7bf762092b27fc0c98a8827bda2dc226bd93
Melanie Boeckmann <melanie.bockmann@gmail.com>
2023-03-14 12:14:55 -0700

    update anatomy content for all components + clean up image paths

--- src/_data/errormessage.js
+++ src/_data/errormessage.js
@@ -2,7 +2,7 @@ module.exports = {
   en : {
     heading: "Handle error messages and validation",
     listitems: [
-      "Use the `required` attribute to activate the required validator. Validation will happen by default on the <onBlur> event. A missing or invalid entry will prompt an inline error message with preset text.",
+      "Use the `required` attribute to activate the required validator. Validation will happen by default on the `onBlur` event. A missing or invalid entry will prompt an inline error message with preset text.",
       "If you need to change the validation event, use the `validate-on` attribute. Validation can happen on blur, when the element loses focus, or manually with the `validate()` method.",
       "Use the `required` attribute for fields that must be filled in. This places \"(required)\" at the end of the label.",
       "Use the `error message` attribute to include an error message text for all required inputs. Avoid using error messages for optional ones.",
