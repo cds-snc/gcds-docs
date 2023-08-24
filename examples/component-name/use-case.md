a91d5c58b06cf2064457baf989618b33bca8c1be
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-08-24 07:59:10 -0400

    Update create-component-page script (#179)
    * Update gen-component-page to match current page layouts
    
    * Update templateKeys

--- examples/component-name/use-case.md
+++ examples/component-name/use-case.md
@@ -1,19 +1,30 @@
 ---
-title: componentName - {Components}
+title: {componentName}
 layout: "layouts/component-documentation.njk"
 eleventyNavigation:
-  key: navKey{locale}
-  title: componentName
+  key: {navKey}{locale}
+  title: {componentName}
   locale: {localeLower}
   parent: components{locale}
-  otherNames: Component
-  description: This is the componentName
-  thumbnail: /images/{localeLower}/{components}/componentName.png
+  otherNames:
+  description: This is the {componentName}
+  thumbnail: /images/common
   alt: This is an image of the component
-translationKey: "navKey"
-tags: ['navKey{locale}', 'usage']
-permalink: /{localeLower}/{components}/componentName/
-date: "git Last Modified"
+  state: coming-soon
+translationKey: "{navKey}"
+tags: ['{navKey}{locale}', 'usage']
+permalink: /{localeLower}/{components}/{componentNameSlug}/
+# date: "git Last Modified"
 ---
 
-## Use case
+## Problems component solves
+
+Use a {componentName}
+
+- 
+
+<article class="bg-full-width bg-primary text-light pt-500 pb-400 my-500">
+  <h2 class="mt-0 mb-400">{relatedComponents}</h2>
+
+  <a href="" class="link-light">component</a>
+</article>
--- scripts/gen-component-page.js
+++ scripts/gen-component-page.js
@@ -2,39 +2,90 @@ const fs = require('fs');
 const path = require('path');
 const replace = require('replace-in-file');
 const prompt = require('prompt-sync')();
+const slugify = require("../utils/slugify");
 
-const englishName = prompt('English name: ');
-const frenchName = prompt('French name: ');
+let englishName = prompt('English name: ');
+let frenchName = prompt('French name: ');
+
+let englishNameSlug =slugify(englishName);
+let frenchNameSlug =slugify(frenchName);
 
 let englishDirectory = './src/en/components/';
 let frenchDirectory = './src/fr/composants/';
 
-copyFolderSync('./examples/component-name', `${englishDirectory}${englishName}`);
-copyFolderSync('./examples/component-name', `${frenchDirectory}${frenchName}`);
+copyFolderSync('./examples/component-name', `${englishDirectory}${englishNameSlug}`);
+copyFolderSync('./examples/component-name', `${frenchDirectory}${frenchNameSlug}`);
 
-fs.rename(`${frenchDirectory}${frenchName}/use-case.md`, `${frenchDirectory}${frenchName}/case-dusage.md`, function(err) {
+fs.rename(`${frenchDirectory}${frenchNameSlug}/use-case.md`, `${frenchDirectory}${frenchNameSlug}/case-dusage.md`, function(err) {
   if ( err ) console.log('ERROR: ' + err);
 });
 
+let templateKeys = [
+  /{componentName}/g,
+  /{componentNameSlug}/g,
+  /{navKey}/g,
+  /{locale}/g,
+  /{alsoCalled}/g,
+  /{componentPreview}/g,
+  /{components}/g,
+  /{localeLower}/g,
+  /{relatedComponents}/g,
+  /{anatomy}/g,
+  /{designA11y}/g,
+  /{buildComponent}/g,
+  /{codeA11y}/g,
+  /{componentNameSlugEN}/g
+];
+
 let engOptions = {
   files: [
-    `${englishDirectory}${englishName}/base.md`,
-    `${englishDirectory}${englishName}/use-case.md`,
-    `${englishDirectory}${englishName}/design.md`,
-    `${englishDirectory}${englishName}/code.md`
+    `${englishDirectory}${englishNameSlug}/base.md`,
+    `${englishDirectory}${englishNameSlug}/use-case.md`,
+    `${englishDirectory}${englishNameSlug}/design.md`,
+    `${englishDirectory}${englishNameSlug}/code.md`
   ],
-  from: [/componentName/g, /globalComponentName/g, /navKey/g, /{locale}/g, /{Components}/g, /{components}/g, /{localeLower}/g],
-  to: [englishName, englishName, englishName.replace('-', ''), "EN", "Components", "components", "en"]
+  from: templateKeys,
+  to: [
+    englishName,
+    englishNameSlug,
+    englishNameSlug.replace('-', ''),
+    "EN",
+    "Also called",
+    `${englishName} component preview`,
+    "components",
+    "en",
+    "Related components",
+    `${englishName} anatomy`,
+    `Design and accessibility for ${englishName}`,
+    `Build a ${englishName}`,
+    `Coding and accessibility for ${englishName}`,
+    englishNameSlug
+  ]
 };
 let frOptions = {
   files: [
-    `${frenchDirectory}${frenchName}/base.md`,
-    `${frenchDirectory}${frenchName}/case-dusage.md`,
-    `${frenchDirectory}${frenchName}/design.md`,
-    `${frenchDirectory}${frenchName}/code.md`
+    `${frenchDirectory}${frenchNameSlug}/base.md`,
+    `${frenchDirectory}${frenchNameSlug}/case-dusage.md`,
+    `${frenchDirectory}${frenchNameSlug}/design.md`,
+    `${frenchDirectory}${frenchNameSlug}/code.md`
   ],
-  from: [/componentName/g, /globalComponentName/g, /navKey/g, /{locale}/g, /{Components}/g, /{components}/g, /{localeLower}/g],
-  to: [frenchName, englishName, englishName.replace('-', ''), "FR", "Composants", "composants", "fr"]
+  from: templateKeys,
+  to: [
+    frenchName,
+    frenchNameSlug,
+    englishNameSlug.replace('-', ''),
+    "FR",
+    "Autres noms ",
+    `Aperçu du composant de ${frenchName}`,
+    "composants",
+    "fr",
+    "Composants connexes",
+    `Structure de la ${frenchName}`,
+    `Accessibilité et design des ${frenchName}`,
+    `Créer une case à ${frenchName}`,
+    `Accessibilité et codage des cases à ${frenchName}`,
+    englishNameSlug
+  ]
 };
 
 try {
