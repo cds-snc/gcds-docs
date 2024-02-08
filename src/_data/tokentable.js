e3d4ff6414afe0107bb530153b88d3c1833a9ef0
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-02-08 12:38:20 -0500

    Turn colour swatches into imgs with aria-labels

---
+++ src/_data/tokentable.js
@@ -0,0 +1,16 @@
+module.exports = {
+  en: {
+    grayscale: 'Gray',
+    blue: 'Blue',
+    red: 'Red',
+    green: 'Green',
+    yellow: 'Yellow',
+  },
+  fr: {
+    grayscale: 'Gris',
+    blue: 'Bleu',
+    red: 'Rouge',
+    green: 'Vert',
+    yellow: 'Jaune',
+  },
+};
