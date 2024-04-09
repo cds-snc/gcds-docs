df3bd734dd073261bb4191d680b1eb4817c6c6b5
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-04-09 11:13:24 -0400

    Reorganize search.js

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,39 +1,9 @@
-function addResult(result) {
-    let results = document.querySelector('#results');
-    
-    let resultElement = document.createElement('section');
-    resultElement.classList.add('search-result');
+import * as pagefind from '/pagefind/pagefind.js';
 
-    let heading = document.createElement('gcds-heading');
-    heading.tag = 'h3';
-    heading.marginBottom = '150';
-    heading.marginTop = '300'
-
-    let link = document.createElement('gcds-link');
-    link.href = result.url;
-    link.innerText = result.meta.title;
-    heading.append(link);
-    resultElement.appendChild(heading);
-
-    let url = document.createElement('gcds-text');
-    url.setAttribute('style', '--gcds-text-role-primary: var(--gcds-color-green-700);');
-    url.marginBottom = '150';
-    url.innerHTML = result.url;
-
-    let excerpt = document.createElement('gcds-text');
-    excerpt.marginBottom = '100';
-    excerpt.innerHTML = result.excerpt;
-
-    resultElement.appendChild(url);
-    resultElement.appendChild(excerpt);
-    results.appendChild(resultElement);
-}
-
-import * as pagefind from "/pagefind/pagefind.js";
 const url = new URLSearchParams(window.location.search);
 
 pagefind.options({
-  baseUrl: "/",
+  baseUrl: '/',
 });
 
 const searchTerm = url.get('q');
@@ -61,12 +31,12 @@ if (searchTerm) {
 
     const totalPages = Math.ceil(results.length / 10);
 
-    let pageResults = results.slice(10*(index-1), (10*index)+1);
-    
+    let pageResults = results.slice(10 * (index - 1), 10 * index + 1);
+
     // length could also be from search.unfilteredResultCount
     for (const result of pageResults) {
-        let data = await result.data();
-        addResult(data);
+      let data = await result.data();
+      addResult(data);
     }
 
     if (totalPages > 1) {
@@ -77,11 +47,45 @@ if (searchTerm) {
       pagination.url = {
         queryStrings: {
           'q': searchTerm,
-          'page::match': '{{1}}' 
-        }
+          'page::match': '{{1}}',
+        },
       };
       pagination.classList.add('mt-400');
       document.getElementById('pagination').appendChild(pagination);
     }
   }
+}
+
+function addResult(result) {
+  let results = document.querySelector('#results');
+
+  let resultElement = document.createElement('section');
+  resultElement.classList.add('search-result');
+
+  let heading = document.createElement('gcds-heading');
+  heading.tag = 'h3';
+  heading.marginBottom = '150';
+  heading.marginTop = '300';
+
+  let link = document.createElement('gcds-link');
+  link.href = result.url;
+  link.innerText = result.meta.title;
+  heading.append(link);
+  resultElement.appendChild(heading);
+
+  let url = document.createElement('gcds-text');
+  url.setAttribute(
+    'style',
+    '--gcds-text-role-primary: var(--gcds-color-green-700);',
+  );
+  url.marginBottom = '150';
+  url.innerHTML = result.url;
+
+  let excerpt = document.createElement('gcds-text');
+  excerpt.marginBottom = '100';
+  excerpt.innerHTML = result.excerpt;
+
+  resultElement.appendChild(url);
+  resultElement.appendChild(excerpt);
+  results.appendChild(resultElement);
 }
