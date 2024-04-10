29b3063cd2ad1ed7eb01fced3d8f5998dda158c9
Melanie Boeckmann <melanie.bockmann@gmail.com>
2024-04-10 10:59:37 -0700

    feat: add main band to footer

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -1,10 +1,22 @@
 module.exports = {
   en: {
+    social: 'Social media',
+    socialurl: 'https://www.canada.ca/en/social.html',
+    mobile: 'Mobile applications',
+    mobileurl: 'https://www.canada.ca/en/mobile.html',
+    about: 'About Canada.ca',
+    abouturl: 'https://design.canada.ca/about/',
     terms: 'Terms and conditions',
     termsurl: 'https://www.canada.ca/en/transparency/terms.html',
     privacy: 'Privacy',
   },
   fr: {
+    social: 'Médias sociaux',
+    socialurl: 'https://www.canada.ca/fr/sociaux.html',
+    mobile: 'Applications mobiles',
+    mobileurl: 'https://www.canada.ca/fr/mobile.html',
+    about: 'À propos de Canada.ca',
+    abouturl: 'https://conception.canada.ca/a-propos/',
     terms: 'Avis',
     termsurl: 'https://www.canada.ca/fr/transparence/avis.html',
     privacy: 'Confidentialité',
