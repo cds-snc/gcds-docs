27d10861fc1e8c9c9141d3544a0dc91a34ad6ca6
Melanie Boeckmann <melanie.bockmann@gmail.com>
2024-05-21 14:27:09 -0700

    refactor: contact us form

--- src/_data/contactus.js
+++ src/_data/contactus.js
@@ -2,58 +2,40 @@ module.exports = {
   en: {
     options: [
       {
-        id: "requestADemo",
-        label: "Request a demo",
-        hint: "Book a demo of GC Design System for your team.",
-        value: "Request a demo"
+        id: 'familiarityUsingGCDS',
+        label: 'I&#39;m currently using GC Design System.',
+        value: 'I&#39;m currently using GC Design System.',
       },
       {
-        id: "reportAnIssue",
-        label: "Report an issue",
-        hint: "Communicate a problem you&#39;ve found.",
-        value: "Report an issue"
+        id: 'familiarityExploringGCDS',
+        label: 'I&#39;ve been exploring GC Design System.',
+        value: 'I&#39;ve been exploring GC Design System.',
       },
       {
-        id: "participateInUserResearch",
-        label: "Participate in user research",
-        hint: "We&#39;ll add your email to a list for user interviews and research.",
-        value: "Participate in user research"
+        id: 'familiarityNeverUsedGCDS',
+        label: 'I&#39;ve never used GC Design System.',
+        value: 'I&#39;ve never used GC Design System.',
       },
-      {
-        id: "other",
-        label: "Other",
-        hint: "Provide a reason in the message field.",
-        value: "Other",
-      }
-    ]
-},
-fr: {
+    ],
+  },
+  fr: {
     options: [
       {
-        id: "requestADemo",
-        label: "Demander une démonstration",
-        hint: "Réservez une démonstration de Système de design GC pour votre équipe.",
-        value: "Request a demo | Demander une démonstration"
+        id: 'familiarityUsingGCDS',
+        label: 'Demander une démonstration',
+        value: 'Request a demo | Demander une démonstration',
       },
       {
-        id: "reportAnIssue",
-        label: "Signaler un problème",
-        hint: "Communiquez un problème que vous avez trouvé.",
-        value: "Report an issue | Signaler un problème"
+        id: 'familiarityExploringGCDS',
+        label: 'Signaler un problème',
+        value: 'Report an issue | Signaler un problème',
       },
       {
-        id: "participateInUserResearch",
-        label: "Participer à la recherche sur les utilisateur·rice·s",
-        hint: "Nous ajouterons votre adresse courriel à la liste des candidat·e·s pour la recherche sur les utilisateur·rice·s.",
-        value: "Participate in user research | Participer à la recherche sur les utilisateur·rice·s"
+        id: 'familiarityNeverUsedGCDS',
+        label: 'Participer à la recherche sur les utilisateur·rice·s',
+        value:
+          'Participate in user research | Participer à la recherche sur les utilisateur·rice·s',
       },
-      {
-        id: "other",
-        label: "Autre",
-        hint: "Indiquez une raison dans le champ du message.",
-        value: "Other | Autre",
-      }
-    ]
+    ],
   },
 };
-  
--- src/en/contact/contact.md
+++ src/en/contact/contact.md
@@ -31,17 +31,26 @@ We’d like to talk to you. Get in touch to request a demo, ask a question, or m
   <input type="hidden" name="form-name" value="contactEN" />
   <input name="honeypot" type="text" aria-label="bot" hidden/>
 
-  <gcds-input type="text" name="name" input-id="name" label="Full name" size="30" autocomplete="name" required></gcds-input>
-  <gcds-input type="email" name="email" input-id="email" label="Email address" size="50" autocomplete="email" required></gcds-input>
-  <gcds-fieldset fieldset-id="reasonForContact" legend="Reason for your communication" required>
-    <gcds-radio-group name="reasonForContact" options='{{ contactus[locale].options | stringify }}'>
+<gcds-input type="text" name="name" input-id="name" label="Full name" size="30" autocomplete="name" required></gcds-input>
+<gcds-input type="email" name="email" input-id="email" label="Email address" size="50" autocomplete="email" required></gcds-input>
+<gcds-textarea name="message" label="Give feedback or ask a question" textarea-id="message"></gcds-textarea>
+
+  <gcds-fieldset fieldset-id="learnMore" legend="Learn more about GC Design System" hint="Choose as many options as you'd like.">
+    <gcds-checkbox checkbox-id="learnMoreMailingList" label="Sign me up for the mailing list." value="Sign me up for the mailing list." name="learnMoreMailingList"></gcds-checkbox>
+    <gcds-checkbox checkbox-id="learnMoreDemo" label="Contact me for a demo." value="Contact me for a demo." name="learnMoreDemo"></gcds-checkbox>
+    <gcds-checkbox checkbox-id="learnMoreResearch" label="Contact me for usability research." value="Contact me for usability research." name="learnMoreResearch"></gcds-checkbox>
+  </gcds-fieldset>
+
+  <gcds-fieldset fieldset-id="familiarityGCDS" legend="Familiarity with the product" hint="Select 1 option." required>
+    <gcds-radio-group name="familiarityGCDS" options='{{ contactus[locale].options | stringify }}'>
     </gcds-radio-group>
   </gcds-fieldset>
-  <gcds-textarea name="message" label="Message" textarea-id="message" hint="Write your question or comment." required></gcds-textarea>
+
   <div hidden>
     <gcds-input type="text" name="bot-field" input-id="bot-field" label="bot"></gcds-input>
   </div>
+
   <gcds-button button-role="primary" type="submit">
-    Send message
+    Submit
   </gcds-button>
 </form>
