84dffaad2d29db97d0eb2c1cf144625b2f67d1a9
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-06-05 09:04:41 -0700

    chore: add comment

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -3,6 +3,8 @@ const getLang =  function (){
     return path.includes('/en/') ? 'EN' : path.includes('/fr/') ? 'FR-CA' : null;
 }
 
+// These are here to set global vars for qualtrics to switch languages
+// So far setting it doesn't work, this needs to be looked at further
 const lang = getLang();
 const currentLang = 'FR-CA';
 console.log('currentLang, lang: ', currentLang, lang)
