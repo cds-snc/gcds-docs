980a67602f743eac197cdd7c7d5989722e4197c3
Brock Higgins <47530176+brockhigg10@users.noreply.github.com>
2025-01-23 12:08:16 -0500

    Update registerdemos.js - Add Feb dates (#465)
    * Update registerdemos.js - Add Feb dates
    
    Added Demo Days for:
    Feb 18 English - 1-2pm
    Feb 25 French - 1-2pm
    
    * Update src/_data/registerdemos.js
    
    Co-authored-by: Ethan Wallace <ethan.wallace.91@gmail.com>
    
    * Update src/_data/registerdemos.js
    
    Co-authored-by: Ethan Wallace <ethan.wallace.91@gmail.com>
    
    * Update src/_data/registerdemos.js
    
    Co-authored-by: Ethan Wallace <ethan.wallace.91@gmail.com>
    
    ---------
    
    Co-authored-by: Ethan Wallace <ethan.wallace.91@gmail.com>

--- src/_data/registerdemos.js
+++ src/_data/registerdemos.js
@@ -12,5 +12,19 @@ module.exports = [
     endtime: "14:00",
     lang: 'fr',
     link: 'https://events.teams.microsoft.com/event/a9bfe87d-4692-465c-a6db-a9c5ef377175@9ed55846-8a81-4246-acd8-b1a01abfc0d1'
+  },
+  {
+    date: "2025-02-18",
+    starttime: "13:00",
+    endtime: "14:00",
+    lang: 'en',
+    link: 'https://events.teams.microsoft.com/event/ca8b7c9d-8fb9-4eaf-8240-c7127b7650a7@9ed55846-8a81-4246-acd8-b1a01abfc0d1'
+  },
+  {
+    date: "2025-02-25",
+    starttime: "13:00",
+    endtime: "14:00",
+    lang: 'fr',
+    link: 'https://events.teams.microsoft.com/event/6a7b57ea-0fbb-4deb-8dd3-b08c5ce3244b@9ed55846-8a81-4246-acd8-b1a01abfc0d1'
   }
 ]
