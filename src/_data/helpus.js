564243707f6c62a923ac9c1072f38a381441ca30
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-02-20 14:12:13 -0500

    Update helpus partial

--- src/_data/helpus.js
+++ src/_data/helpus.js
@@ -2,9 +2,11 @@ module.exports = {
   en: {
     heading: 'Help us improve',
     paragraph:
-      "Have questions? Something you'd change or you'd like to see? Share your feedback with us to help improve GC Design System.",
+      "Have questions or a request? Give feedback on our contact form.",
     feedback: 'Give feedback',
     feedbackHref: '/en/contact/#contact-us',
+    report: 'Report an issue on GitHub',
+    opens: ' (Opens destination in a new tab.)',
     github: 'Get involved on GitHub',
   },
   fr: {
@@ -13,6 +15,8 @@ module.exports = {
       'Avez-vous des questions? Y a-t-il quelque chose que vous changeriez ou ajouteriez? Faites-nous part de vos commentaires pour nous aider à améliorer Système de design GC.',
     feedback: 'Fournir des commentaires',
     feedbackHref: '/fr/contactez/#contactez-nous',
+    report: 'Report an issue on GitHub',
+    opens: ' (Ouvre l\'emplacement dans un nouvel onglet.)',
     github: "S'impliquer sur GitHub",
   },
 };
