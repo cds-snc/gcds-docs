d78783d5992a1dcb8b7ea5688180f41be59e1273
Pat Heard <patrick.heard@cds-snc.ca>
2025-07-25 15:08:21 -0400

    fix: switch to capture to ensure sanitization occurs before submission

--- src/scripts/sanitize-contact-form.js
+++ src/scripts/sanitize-contact-form.js
@@ -4,13 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
   // If the contact form exists, remove PII from textareas before submission
   if (contactForm) {
     contactForm.addEventListener('submit', function (event) {
-      event.preventDefault();
       const textareas = contactForm.querySelectorAll('gcds-textarea');
 
       textareas.forEach(textarea => {
         textarea.value = sanitizePii(textarea.value);
       });
-      contactForm.submit();
-    });
+    }, { capture: true });
   }
 });
