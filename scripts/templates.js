4c168f7529603e40f77503ff742af0f609b15790
Daine Trinidad <daine.trinidad@cds-snc.ca>
2025-10-01 14:19:43 -0700

    chore: name change UF to CS

--- scripts/templates.js
+++ scripts/templates.js
@@ -17,8 +17,8 @@ async function downloadFile(url, outputDir, outputFileName) {
 
     // Replace version numbers in preview files
     let previewContent = fileContent.replace(
-      /gcds-utility\@<version-number>/g,
-      'gcds-utility@' +
+      /gcds-css-shortcuts\@<version-number>/g,
+      'gcds-css-shortcuts@' +
         pjson.devDependencies['@gcds-core/css-shortcuts'].replace(/^\^/, ''),
     );
     previewContent = previewContent.replace(
