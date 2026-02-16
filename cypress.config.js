1fad01afaaded55107552f2e10af3b5f90454667
Ethan Wallace <ethan.wallace.91@gmail.com>
2026-02-16 08:35:47 -0500

    tests: add retires to solve timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,29 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
         log(message) {
-          console.log(message)
-    
-          return null
+          console.log(message);
+
+          return null;
         },
         table(message) {
-          console.table(message)
-    
-          return null
-        }
-      })
+          console.table(message);
+
+          return null;
+        },
+      });
     },
     baseUrl: 'http://localhost:8080',
     viewportWidth: 1280,
     viewportHeight: 850,
     screenshotOnRunFailure: false,
     video: false,
+    retries: {
+      runMode: 3,
+      openMode: 0,
+    },
   },
 });
