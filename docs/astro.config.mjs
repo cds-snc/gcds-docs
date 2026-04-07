6135044c39d4d3c8d0150a939a01196a17f38fec
Daine Trinidad <daine.trinidad@cds-snc.ca>
2026-04-07 13:18:02 -0700

    chore: init files

---
+++ docs/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  i18n: {
+    locales: ["en-CA", "fr-CA"],
+    defaultLocale: "en-CA",
+  },
+  i18n: {
+    locales: ["en-CA", "fr-CA"],
+    defaultLocale: "en-CA",
+  },
+  integrations: [mdx(), react()],
+});
---
+++ docs/src/i18n/config.ts
@@ -0,0 +1,23 @@
+export const routes = {
+  home: {
+    en: "",
+    fr: "",
+  },
+  about: {
+    en: "about-us",
+    fr: "a-propos",
+  },
+  components: {
+    en: "components",
+    fr: "composants",
+    children: {
+      button: {
+        en: "button",
+        fr: "bouton",
+      },
+    },
+  },
+};
+
+export const defaultLocale = "en";
+export const supportedLocales = ["en", "fr"];
