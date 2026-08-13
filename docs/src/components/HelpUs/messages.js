602002dc76eb96bd77924fd953e85df5c0e864c8
Ethan Wallace <daine.trinidad@cds-snc.ca>
2026-08-13 11:56:18 -0400

    feat(astro): Migrate contact pages (#923)

--- docs/src/components/HelpUs/messages.js
+++ docs/src/components/HelpUs/messages.js
@@ -1,9 +1,10 @@
+import { links } from "~i18n/links";
 export const messages = {
   en: {
     heading: "Help us improve",
     text: [
       "Have questions or a request? Give feedback on our contact form.",
-      "Something's wrong? Raise it through GitHub with an account. You'll have access to the team's direct responses, progress made on your issue, and issues raised by others.",
+      `Something's wrong? Raise it through GitHub with an <gcds-link href={${links.en.githubGetStarted}} external>account</gcds-link>. You'll have access to the team's direct responses, progress made on your issue, and issues raised by others.`,
     ],
     btnFeedback: "Give feedback",
     btnFeedbackHref: "/en/contact/#contact-us",
@@ -14,7 +15,7 @@ export const messages = {
     heading: "Aidez-nous à nous améliorer",
     text: [
       "Vous avez des questions ou des demandes? Faites-nous part de vos commentaires à l'aide de notre formulaire de contact.",
-      "Quelque chose ne va pas? Signalez-le sur GitHub en créant un compte. Vous pourrez accéder aux réponses de l'équipe, suivre les progrès réalisés concernant votre problèmes et voir les problèmes signalés par d'autres personnes.",
+      `Quelque chose ne va pas? Signalez-le sur GitHub en créant un <gcds-link href={${links.fr.githubGetStarted}} external>compte</gcds-link>. Vous pourrez accéder aux réponses de l'équipe, suivre les progrès réalisés concernant votre problèmes et voir les problèmes signalés par d'autres personnes.`,
     ],
     btnFeedback: "Fournir des commentaires",
     btnFeedbackHref: "/fr/contactez/#contactez-nous",
