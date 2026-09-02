eb953c2b4457e4bc755daad6c0b65195b767fe89
Ethan Wallace <ethan.wallace.91@gmail.com>
2026-09-02 14:49:24 -0400

    feat(astro): Migrate start to use pages (#921)
    * feat(astro): Migrate start to use pages
    
    * Update layout path
    
    * PR feedback

---
+++ docs/src/components/AddCssShortcuts/messages.js
@@ -0,0 +1,18 @@
+export const messages = {
+  en: {
+    heading: 'Add CSS Shortcuts stylesheet',
+    paragraphs: [
+      'We recommend using <gcds-link href="/en/css-shortcuts">GC Design System CSS Shortcuts</gcds-link> to apply predefined CSS utility classes. These classes align with our components and help you build faster while maintaining consistency.',
+      'Using CSS Shortcuts in your project is optional. It can be added alongside your existing CSS.',
+      'Navigate to your project’s root folder and run the following command:',
+    ],
+  },
+  fr: {
+    heading: 'Ajouter la feuille de style des Raccourcis CSS ',
+    paragraphs: [
+      'Nous recommandons d’utiliser les <gcds-link href="/fr/raccourcis-css">raccourcis CSS de Système de design GC</gcds-link> pour appliquer des classes utilitaires CSS prédéfinies. Ces classes sont conformes à nos composants et aident à construire plus rapidement tout en assurant l’uniformité.',
+      'L’utilisation des Raccourcis CSS dans votre projet n’est pas obligatoire. Ils peuvent être ajoutés à votre CSS existant.',
+      'Naviguez vers le dossier racine de votre projet et exécutez la commande suivante :',
+    ],
+  },
+};
