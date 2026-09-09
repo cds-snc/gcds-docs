1d5a46181be7d14378e4196af02df3916fda7851
Melanie Boeckmann <melanie.bockmann@gmail.com>
2026-09-09 07:43:03 -0700

    feat(astro):render component previews in iframes

--- docs/src/components/index.ts
+++ docs/src/components/index.ts
@@ -5,6 +5,7 @@ export { default as CdnVersionScript } from "./CdnVersionScript.astro";
 export { default as CodePreview } from "./CodePreview.astro";
 export { default as CodePreviewReact } from "./CodePreviewReact.jsx";
 export { default as ComponentPreview } from "./ComponentPreview.astro";
+export { default as PreviewFrame } from "./PreviewFrame.astro";
 export { default as TabNav } from "./TabNav.astro";
 export { default as ContactForm } from "./ContactForm/ContactForm.astro";
 export { default as DemoDates } from "./DemoDates/DemoDates.astro";
